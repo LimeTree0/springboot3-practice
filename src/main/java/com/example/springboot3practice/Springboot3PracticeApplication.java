9b10d3bab1eeb5ca2305b54e949f5f00b96adba4
LimeTree0 <bum9516@gmail.com>
2023-08-11 09:57:40 +0900

    Initial commit

---
+++ src/main/java/com/example/springboot3practice/Springboot3PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3PracticeApplication.class, args);
+    }
+
+}
